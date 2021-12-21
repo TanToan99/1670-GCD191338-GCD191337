dfe43f37c4db7c70fb0b71e81290f61e3ec2675f
TanToan99 <tantoan1909@gmail.com>
2021-12-21 22:41:16 +0700

    Feat Middleware & Fix Route
    Add Role check middleware
    Fix route namespace

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
